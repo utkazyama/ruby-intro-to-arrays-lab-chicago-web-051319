1dc4e12a6b10f0b52b4e27d1c6a549964beb3fe8
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-07 05:59:20 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,18 @@
+def instantiate_new_array
+  array=[]
+end
+
+def array_with_two_elements
+  arrray=["a","b"]
+end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array,index_number)
+  array[2]
+end
+
+def 
+
